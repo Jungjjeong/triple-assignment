eafdb1111b2d24efd60e7482290d952b4671fa63
Jungjjeong <sky990106@dgu.ac.kr>
2022-07-06 22:21:26 +0900

    Feat: onEaseOutExpo util 함수 구현
    - 기하급수적(Exponential) easing out 기능 구현 및 적용

--- src/hooks/useCountUp.ts
+++ src/hooks/useCountUp.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 
+import onEaseOutExpo from '../utils/onEaseOutExpo'
+
 const useCountUp = (start: number, end: number, duration: number) => {
   const [count, setCount] = useState(start)
   const stepTime = 1000 / 30
@@ -10,7 +12,7 @@ const useCountUp = (start: number, end: number, duration: number) => {
 
     const countInterval = setInterval(() => {
       currentNumber += 1
-      const progress = currentNumber / totalStepTime
+      const progress = onEaseOutExpo(currentNumber / totalStepTime, 1)
       setCount(Math.round(end * progress))
 
       if (progress === 1) {
---
+++ src/utils/onEaseOutExpo.ts
@@ -0,0 +1,10 @@
+const onEaseOutExpo = (t: number, d: number) => {
+  return t === d ? 1 : -Math.pow(2, (-10 * t) / d) + 1 + 0
+}
+
+// T: 경과한 시간
+// b: 시작 값 (0)
+// c: 변화 량 (1)
+// d: 애니메이션에 소요되는 시간
+
+export default onEaseOutExpo
