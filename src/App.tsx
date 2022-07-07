62a6e0a44d99b7ffd1ee822ed0e934a131f06384
Jungjjeong <sky990106@dgu.ac.kr>
2022-07-08 02:38:03 +0900

    Feat: App Component Header Text 추가
    - 배포를 위해 Header Text 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import styled from '@emotion/styled'
+
 import { TripleSection } from './components'
+
+const HeaderText = styled.h1`
+  width: fit-content;
+  margin: 50px auto;
+  font-size: 3rem;
+`
 
 const App = () => {
   return (
     <div>
-      <header>Triple Frontend Practice</header>
+      <header>
+        <HeaderText>Triple Frontend Practice</HeaderText>
+      </header>
       <section>
         <TripleSection />
       </section>
